5abbc8a50c997cdf91f498e4e0e6d4635a8d8af6
Luat Nguyen <luatnguyen@higgsvalley.com>
2017-07-24 02:10:43 +0700

    Add Drone166 animation timeline: Go forward -> rotate -> Go back -> rotate -> Do all again

--- src/AFrame/Decorator/Center.jsx
+++ src/AFrame/Decorator/Center.jsx
@@ -8,12 +8,17 @@ import obj_Drone166_dae from "../../../assets/obj/Drone166/Drone166x.dae";
 export class Center extends React.Component {
 
   render() {
+    
     return (
       <Entity {...this.props} className="decoratorLocation center" position="0 0 0">
-        {/*<Entity collada-model={obj_Drone166_dae} position="5 1.5 -3" rotation="0 0 0"/>*/}
-        {/*<Entity collada-model={obj_Drone166_dae} position="5 1.5 2" rotation="0 180 0"/>*/}
-        <Entity collada-model={'#obj_Drone166_dae'} position="-5 1.5 2" rotation="0 180 0"/>
-        <Entity collada-model="#obj_Drone166_dae" position="-5 1.5 -2" rotation="0 0 0"/>
+        {/*<Entity collada-model="#obj_Drone166_dae" position="5 1.5 -3" rotation="0 0 0"/>*/}
+        {/*<Entity collada-model="#obj_Drone166_dae" position="5 1.5 2" rotation="0 180 0"/>*/}
+        {/*<Entity collada-model="#obj_Drone166_dae" position="-5 1.5 2" rotation="0 180 0"/>*/}
+        
+        <Entity collada-model="#obj_Drone166_dae" position="-5 1.5 -10" rotation="0 0 0">
+          <a-animation delay="1000" dur="10000" attribute="position" from="-5 1.5 -10" to="-5 1.5 0" repeat="indefinite" direction="alternate" />
+          <a-animation delay="9000" dur="2000"  attribute="rotation" to="0 180 0"  repeat="indefinite" direction="alternate" />
+        </Entity>
       </Entity>
     );
   }
