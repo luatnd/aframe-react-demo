b204b211fed1bf42cbee6e2f02953ff44c20db92
Luat Nguyen <luatnguyen@higgsvalley.com>
2017-08-17 18:47:52 +0700

    Change 0 screen content

--- src/VR_InteractionUI/HtmlShader/ScreenContent/ControlPanel.jsx
+++ src/VR_InteractionUI/HtmlShader/ScreenContent/ControlPanel.jsx
@@ -12,6 +12,12 @@ const styles = theme => ({
   },
   title: {fontSize: '42px', lineHeight: '60px',color: 'rgb(65,177,177)',},
   paragraph: {fontSize: '28px', lineHeight: '40px', color: 'rgb(65,177,177)',},
+  
+  list: {
+    width: '100%',
+    maxWidth: 360,
+    background: theme.palette.background.paper,
+  },
 });
 
 @withStyles(styles)
@@ -31,13 +37,18 @@ export default class ControlPanel extends React.Component {
           Hello, HTML!
         </Typography>
         <Typography gutterBottom noWrap className={classes.paragraph}>
-          TODO: Improve React rendering performance
+          • Use your keyboard ◀ ▶ ▲ ▼ and mouse to move<br/>
+          • You have an small circle cursor at the center of screen,<br/>
+            help you interact with objects with mouse click, try to find it<br/>
+        </Typography>
+        
+        
+        
+        <Typography gutterBottom noWrap className={classes.paragraph}>
+          TODO: Improve React rendering performance via Pure Component
         </Typography>
         <Typography gutterBottom noWrap className={classes.paragraph}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          TODO: Improve animation performance: Do animation when needed only
         </Typography>
       </div>
     );
